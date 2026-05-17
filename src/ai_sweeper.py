6c9392cdf1e5cb8a117e965e6d10321f61afd6ae
Giovana Iantsch <165342038+giovanaiantsch@users.noreply.github.com>
2026-05-17 12:36:16 -0300

    Initial commit

---
+++ src/ai_sweeper.py
@@ -0,0 +1,6 @@
+# solução final 
+def prever_jogada_segura(tabuleiro_atual, modelo):
+    # 1. Faça o pré-processamento do tabuleiro_atual (ETL)
+    # 2. Peça ao 'modelo' para fazer a predição (.predict)
+    # 3. Retorne a coordenada X e Y mais segura
+    return (x, y)
